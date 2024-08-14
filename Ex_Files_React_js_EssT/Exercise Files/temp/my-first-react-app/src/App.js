e1296d1fc79f0a0fcf4ba928a462f888b37b98ed
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2024-08-14 11:49:01 -0600

    Adding mising files

---
+++ Ex_Files_React_js_EssT/Exercise Files/temp/my-first-react-app/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+
+function Home() {
+  return (
+    <div>
+      <h1>My Home</h1>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h1>Mad about me</h1>
+    </div>
+  );
+}
+
+function Contact() {
+  return (
+    <div>
+      <h1>Contact us</h1>
+    </div>
+  );
+}
+
+
+function App() {
+  return <Home/>;
+}
+
+export default App;
